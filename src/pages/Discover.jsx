6d20c35f3fc8bf9bbffcc39aaaa9672a2139bc44
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2022-11-16 22:13:26 +0600

    Song card component & play/pause

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,12 +1,16 @@
-import { Error, Loader, SongCard } from '../components';
+import { useDispatch, useSelector } from 'react-redux';
 import { genres } from '../assets/constants';
+import { Error, Loader, SongCard } from '../components';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
-  const {data, isFetching, error} = useGetTopChartsQuery();
+    const dispatch = useDispatch();
+    const {activeSong, isPlaying} = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
 
-  console.log(data)
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error />;
 
   return (
     <div className="flex flex-col">
@@ -22,11 +26,14 @@ const Discover = () => {
       </div>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard
             key={song.key}
             song={song}
             i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
           />
         ))}
       </div>
