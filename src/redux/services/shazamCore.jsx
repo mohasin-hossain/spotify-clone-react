6a52a00e639073040128440cf7bc164f07beddd2
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2022-11-16 20:24:22 +0600

    Api setup

---
+++ src/redux/services/shazamCore.jsx
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '3e3b2505fdmsh2387870c3b87252p166658jsnb337fabbb05b');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
